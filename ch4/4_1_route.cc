3e6e6df7ec4558d81d5f4e1ea23eb26d337ae2e7
abagogogo <rcnjko@yahoo.com.tw>
2018-10-09 07:41:14 +0800

    4.1 revised

--- ch4/4_1_route.cc
+++ ch4/4_1_route.cc
@@ -2,26 +2,53 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-bool has_route(Graph &g, int start, int end, vector<bool> &visited, vector<bool> &predece) {
+#if 0 // DFS
+bool has_route(Graph &g, int start, int end, vector<bool> &visited) {
+    visited[start] = true;
+    if (start == end) return true;
+
     vector<int> list = g.get_neighbors(start);
     for (int next : list) {
-        if (next == end) return true;
-        if (visited[next]) continue;
-        visited[next] = true;
-        if (has_route(g, next, end, visited, predece)) return true;
+        if (!visited[next]) {
+            if (has_route(g, next, end, visited)) 
+                return true;
+        }
     }
     return false;
 }
 
 bool has_route(Graph &g, int start, int end) {
     vector<bool> visited(g.get_node_cnt(), false);
-    vector<bool> predece(g.get_node_cnt(), false);
-
-    return has_route(g, start, end, visited, predece);
+    return has_route(g, start, end, visited);
 }
+#else // BFS
+
+bool has_route(Graph &g, int start, int end) {
+    vector<bool> mark(g.get_node_cnt(), false);
+    deque<int> q;
+
+    mark[start] = true;
+    q.push_back(start);
+    
+    while (!q.empty()) {
+        int curr = q.front(); q.pop_front();
+        if (curr == end) return true;
+
+        vector<int> list = g.get_neighbors(curr);            
+        for (int next : list) {
+            if (!mark[next]) {
+                mark[next] = true;
+                q.push_back(next);
+            }
+        }
+    }
+    return false;
+}
+#endif
 
 int main(void) {
     Graph g1;
@@ -30,6 +57,8 @@ int main(void) {
     g1.add_edge(0, 2);
     g1.add_edge(1, 4);
     g1.add_edge(2, 3);
+    g1.add_edge(4, 0);
+    g1.add_edge(1, 0);
     g1.print();
 
     cout << "from 0 to 3: " << (has_route(g1, 0, 3) ? "TRUE" : "FALSE") << endl;
