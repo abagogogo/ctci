f782d70f0755886324be7a547ddfb84199ce3f1e
abagogogo <rcnjko@yahoo.com.tw>
2018-10-18 08:46:03 +0800

    revise 8.11

--- ch8/8_11_coins.cc
+++ ch8/8_11_coins.cc
@@ -2,12 +2,10 @@
 
 using namespace std;
 
-#if 1 // memoization.
-int make_change(int amount, const vector<int> &denoms, int index, map<pair<int, int>, int> &memo) {
+int make_change(int amount, const vector<int> &denoms, int index, vector<vector<int>> &memo) {
     if (index >= denoms.size() - 1) return 1;
 
-    pair<int, int> key = {amount, index};
-    if (memo[key] > 0) return memo[key];
+    if (memo[amount][index] != 0) return memo[amount][index];
 
     int denom_amount = denoms[index];
     int ways = 0;
@@ -16,32 +14,17 @@ int make_change(int amount, const vector<int> &denoms, int index, map<pair<int, 
         int remain_amount = amount - i * denom_amount;
         ways += make_change(remain_amount, denoms, index + 1, memo);
     }
-    memo[key] = ways;
+    memo[amount][index] = ways;
     return ways; 
 }
 
 int make_change(int amount) {
     vector<int> denoms = {25, 10, 5, 1};
-    map<pair<int, int>, int> memo;
+    //map<pair<int, int>, int> memo; // unordered_map cannot take pair as key.
+    vector<vector<int>> memo(amount + 1, vector<int>(denoms.size(), 0));
 
     return make_change(amount, denoms, 0, memo);
 }
-#else // DP: NG
-int make_change(int amount) {
-    vector<int> denoms = {25, 10, 5, 1};
-    map<int, int> ways;
-
-    ways[0] = ways[1] = 1;
-    for (int i = 2; i <= amount; ++i) {
-        for (auto denom : denoms) {
-            if (i >= denom) {
-                ways[i] += ways[i - denom];
-            }
-        }
-    }
-    return ways[amount];
-}
-#endif
 
 int main(void) {
     
